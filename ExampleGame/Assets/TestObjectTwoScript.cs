ec06172f037409f943c6f129471a0e609ab24038
canboy20 <can.atay20@gmail.com>
2020-05-23 20:42:50 +0400

    Create Script 2. Update Scene

---
+++ ExampleGame/Assets/TestObjectTwoScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObjectTwoScript : MonoBehaviour
+{
+
+    string scriptName = "TestObjectOne";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scriptName += "_2";
+
+    }
+}
